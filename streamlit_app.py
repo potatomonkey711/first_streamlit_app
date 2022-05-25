d39b7ee46a5bcae16474ba7e6ad5c72369c04595
potatomonkey711 <37160870+potatomonkey711@users.noreply.github.com>
2022-05-25 17:44:06 +1000

    Update streamlit_app.py
    Added api call for fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -18,3 +19,6 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # display as table
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
