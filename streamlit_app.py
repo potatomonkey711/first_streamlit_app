d7696d3bb530df94cf308a3c896654271030bc89
potatomonkey711 <37160870+potatomonkey711@users.noreply.github.com>
2023-05-28 16:16:41 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -21,3 +22,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+
+
+streamlit.header("Fruityvice Fruit Advice!")
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+# normalize the JSON received from the GET request
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display the normalized JSON in a dataframe
+streamlit.dataframe(fruityvice_normalized)
